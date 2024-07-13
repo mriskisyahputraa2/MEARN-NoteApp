b125715b4a5fde2e4e6267ef8cdf1951c892ef8b
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-07-13 12:18:45 +0700

    update

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import PasswordInput from "../../components/Input/PasswordInput";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosinstance";
 
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  // const [success, setSuccess] = useState(false); // Menambahkan state untuk pesan sukses
+  const navigate = useNavigate();
 
   // validasi form
   const handleSignUp = async (e) => {
@@ -31,6 +34,59 @@ const SignUp = () => {
     setError(""); // jika data sudah berhasil dimasukkan, jangan tampilkan pesan errornya
 
     // SignUp API
+
+    try {
+      // mengirim permintaan post ke endpoint "create-account"
+      const response = await axiosInstance.post("create-account", {
+        // dengan fullName, email dan password
+        fullName: name,
+        email: email,
+        password: password,
+      });
+
+      if (response.data && response.data.error) {
+        setError(response.data.message);
+        return;
+      }
+
+      // validasi, jika response data dan response data message ada
+      if (response.data && response.data.message) {
+        alert(response.data.message); // menampilkan pesan "register berhasil"
+        navigate("/login"); // mengarahkan pengguna ke halaman login setelah berhasil register
+      }
+      // if (response.data && response.data.message === "Register Successful") {
+      //   setSuccess(true); // Menampilkan pesan sukses
+      //   setTimeout(() => {
+      //     navigate("/login"); // Mengarahkan pengguna ke halaman login setelah 2 detik
+      //   }, 2000);
+      // }
+
+      // if (response.data && response.data.error) {
+      //   setError(response.data.message);
+      //   return;
+      // }
+
+      // // validasi, jika response data dan response data access token ada
+      // if (response.data && response.data.accessToken) {
+      //   localStorage.setItem("token", response.data.accessToken); // token akan disimpan dalam localStorage dengan key "Token"
+      //   navigate("/dashboard"); // setelah berhasil, maka pengguna akan dibawa kehalaman dashboard
+      // }
+
+      //  jika terjadi error selama autentikasi
+    } catch (error) {
+      // validasi, jika semua response error ini ada
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        // maka pesan kesalahan dari server ditetapkan ke state error menggunakan fungsi setError.
+        setError(error.response.data.message);
+      } else {
+        // jika tidak, maka tampilkan pesan "coba lagi"
+        setError("An unexpected error occurred, Please try again.");
+      }
+    }
   };
 
   return (
